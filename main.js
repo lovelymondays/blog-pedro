2b9ec3006119c12512194761fb43785c8369729f
lovelymondays <arjism23@gmail.com>
2024-06-29 16:28:09 +0700

    update

--- main.js
+++ main.js
@@ -202,7 +202,7 @@ var btnSubmit = document.querySelector(".submit");
 var modal = document.querySelector(".modal-container");
 if (btnSubmit) {
   btnSubmit.addEventListener("click", function () {
-    modal.classList.add("show");
+    modal.classList.add(".show");
     const next = document.querySelector(".next");
     next.setAttribute("value", "https://arjism.online/contact.html");
   });
